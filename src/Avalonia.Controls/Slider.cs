3de092c31339a7c37c9bae3050e9f314f80e29a3
Steven Kirk <grokys@gmail.com>
2020-11-04 19:06:59 +0100

    Implemented more automation on win32
    - Button invoke
    - Raise events due to properties changing
    - Fix control bounds

--- src/Avalonia.Controls/Slider.cs
+++ src/Avalonia.Controls/Slider.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Collections;
+using Avalonia.Controls.Automation.Peers;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Mixins;
 using Avalonia.Controls.Primitives;
@@ -184,6 +185,8 @@ namespace Avalonia.Controls
 
             _pointerMovedDispose = this.AddDisposableHandler(PointerMovedEvent, TrackMoved, RoutingStrategies.Tunnel);
         }
+
+        protected override AutomationPeer OnCreateAutomationPeer() => new SliderAutomationPeer(this);
 
         private void TrackMoved(object sender, PointerEventArgs e)
         {
